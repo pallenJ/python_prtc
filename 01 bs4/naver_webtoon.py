a90564142c6ede5c2ad4f67f955f3f411ce60faf
pallenJ <pallennellap17@gmail.com>
2020-03-08 19:43:26 +0900

    for문 수정

--- 01 bs4/naver_webtoon.py
+++ 01 bs4/naver_webtoon.py
@@ -7,9 +7,12 @@ soup = bs(html.text,'html.parser')
 data1 = soup.findAll('div',{'class':'col_inner'})[6]
 data2 = data1.findAll('a',{'class':'title'})
 
-title_list = []
-for t in data2 :
-    title_list.append(t.text);
+
+# title_list = []
+# for t in data2 :
+#     title_list.append(t.text);
+title_list = [t.text for t in data2]
+
 
 pprint(title_list)
 html.close();
