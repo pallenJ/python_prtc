1e86707986e1ac29fd26cf509f7b9d2f642c413e
pallenJ <pallennellap17@gmail.com>
2020-03-10 17:38:21 +0900

    1 to 50 자동화 완료

--- 02 selenium/1to50.py
+++ 02 selenium/1to50.py
@@ -1,11 +1,22 @@
-import time
-
 from selenium import webdriver
 
 driver = webdriver.Chrome('chromedriver')
 driver.get('http://zzzscore.com/1to50')
 driver.implicitly_wait(300)
 
-btns = driver.find_elements_by_xpath('//*[@id="grid"]/div[*]')
-#print(len(btns))
-print(btns[0].text)
+num =1
+
+def clickBtn():
+    # 1 to 50 게임의 각각의 버튼 가져옴
+    global num
+    btns = driver.find_elements_by_xpath('//*[@id="grid"]/div[*]')
+
+    for btn in btns:
+        if btn.text == str(num): # 문자열로 형변환후 비교
+            btn.click()
+            num+=1
+            return
+
+## main code
+while num<=50:
+    clickBtn()
