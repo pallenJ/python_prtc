624e9c7c4300cf90326f1c92c737e8fc71ed5787
pallenJ <pallennellap17@gmail.com>
2020-03-10 17:26:57 +0900

    1 to 50 페이지 분석 및 버튼 가져오기

--- 02 selenium/1to50.py
+++ 02 selenium/1to50.py
@@ -1,21 +1,11 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('chromedriver')
-driver.get('https://www.youtube.com/')
+driver.get('http://zzzscore.com/1to50')
+driver.implicitly_wait(300)
 
-time.sleep(3)
-
-# 검색어창 부분
-# XPATH : selenium 에서 특정한 요소를 가리키는 경로 지정법
-# //*[@id="search"]는 유튜브 검색창을 XPATH로 계산된 경로
-search = driver.find_element_by_xpath('//*[@id="search"]')
-
-# search 변수에 저장된 곳에 값 전송
-search.send_keys('무다무다')
-time.sleep(1)
-
-#엔터나 방향키등 특수키는 Keys.원하는 키 로 가능. 엔터의 경우 아래와 같음
-search.send_keys(Keys.ENTER)
+btns = driver.find_elements_by_xpath('//*[@id="grid"]/div[*]')
+#print(len(btns))
+print(btns[0].text)
