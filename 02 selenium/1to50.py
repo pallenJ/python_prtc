5d1abbf73753ca72ca7a876d7711ca436c47ba80
pallenJ <pallennellap17@gmail.com>
2020-03-10 17:13:05 +0900

    유튜브 키워드 검색 완료

--- 02 selenium/1to50.py
+++ 02 selenium/1to50.py
@@ -1,3 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('chromedriver')
+driver.get('https://www.youtube.com/')
+
+time.sleep(3)
+
+# 검색어창 부분
 # XPATH : selenium 에서 특정한 요소를 가리키는 경로 지정법
-# 검색창에 XPATH로 계산된 경로
-#//*[@id="search"]
+# //*[@id="search"]는 유튜브 검색창을 XPATH로 계산된 경로
+search = driver.find_element_by_xpath('//*[@id="search"]')
+
+# search 변수에 저장된 곳에 값 전송
+search.send_keys('무다무다')
+time.sleep(1)
+
+#엔터나 방향키등 특수키는 Keys.원하는 키 로 가능. 엔터의 경우 아래와 같음
+search.send_keys(Keys.ENTER)
