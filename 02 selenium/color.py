9920b4aaf80d615c0e835347d7be81171df7c09b
pallenJ <pallennellap17@gmail.com>
2020-03-12 12:20:33 +0900

    색맹테스트 구현. 단 최적화가 필요.

--- 02 selenium/color.py
+++ 02 selenium/color.py
@@ -1,14 +1,45 @@
-import pprint
-
+import time
+from collections import Counter
+from pprint import pprint
 from selenium import webdriver
+
 
 driver = webdriver.Chrome('chromedriver')
 driver.get('http://zzzscore.com/color')
 driver.implicitly_wait(300)
 
-btns = driver.find_elements_by_xpath('//*[@id="grid"]/div')
-# print(len(btns))
 
-# 백그라운드 컬러 추출
-btns_rgba = [btn.value_of_css_property('background-color') for btn in btns]
-pprint(btns_rgba)
+# print(len(btns))
+btns = driver.find_elements_by_xpath('//*[@id="grid"]/div')
+
+
+def analysis():
+
+    btns_rgba = [btn.value_of_css_property('background-color') for btn in btns]
+    # 백그라운드 컬러 추출
+    #pprint(btns_rgba)
+
+    result = Counter(btns_rgba)
+    pprint(result)
+
+    #값이 1인것 탐색
+    for key, value in result.items():
+        if value == 1:
+            answer = key
+            break
+        else:
+            answer = None
+            print("정답을 찾을 수 없습니다.")
+
+    # 정답클릭
+    # 1. btns_rgba에서 인덱스값을 구함
+    # 2. 해당 값으로 btns에 접근
+    if answer :
+        index = btns_rgba.index(answer)# indexOf와 같은 역할.
+        btns[index].click();
+
+## main
+start = time.time()
+while time.time() - start <=60:
+    analysis()
+
