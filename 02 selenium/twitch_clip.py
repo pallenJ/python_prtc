52931b2acfed2f6df0006aedd18068346945dffb
pallenJ <pallennellap17@gmail.com>
2020-03-13 14:53:13 +0900

    영상 다운로드까지 완료

--- 02 selenium/twitch_clip.py
+++ 02 selenium/twitch_clip.py
@@ -1,4 +1,6 @@
+import re
 import time
+from urllib.request import urlretrieve
 
 from selenium import webdriver
 
@@ -20,7 +22,6 @@ vid_title, vid_date = None,None
 for span in title_element2:
     try:
         d_type = span.get_attribute('data-test-selector')
-        print(span.text)
         if d_type == 'title':
             vid_title = span.text
         elif d_type == 'date':
@@ -28,4 +29,13 @@ for span in title_element2:
     except:
         pass
 
+
+# 정규식으로 특수문자 처리
+vid_date = re.sub('[^0-9a-zA-Zㄱ-힗]','',vid_date)
+vid_title = re.sub('[^0-9a-zA-Zㄱ-힗]','',vid_title)
+
 print(vid_title,vid_date)
+
+urlretrieve(vid_url, vid_title+'_'+vid_date+'.mp4')
+
+driver.close()
