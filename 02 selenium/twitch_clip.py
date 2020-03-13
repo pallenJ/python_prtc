0661bbf4f14af0ce5d3e8f1327d9a2fd0c4a63f4
pallenJ <pallennellap17@gmail.com>
2020-03-13 14:46:41 +0900

    영상제목/날짜 가져오기 완료

--- 02 selenium/twitch_clip.py
+++ 02 selenium/twitch_clip.py
@@ -12,5 +12,20 @@ time.sleep(3)
 
 url_element = driver.find_element_by_tag_name('video') # 가져올 태그명
 vid_url = url_element.get_attribute('src') # 가져올 속성명 
-print(vid_url)
 
+title_element1 = driver.find_element_by_class_name('tw-flex')
+title_element2 = driver.find_elements_by_tag_name('span')
+vid_title, vid_date = None,None
+
+for span in title_element2:
+    try:
+        d_type = span.get_attribute('data-test-selector')
+        print(span.text)
+        if d_type == 'title':
+            vid_title = span.text
+        elif d_type == 'date':
+            vid_date = span.text
+    except:
+        pass
+
+print(vid_title,vid_date)
