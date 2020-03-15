94da2e87a5d5475069449f1ee801881a7f99d95e
pallenJ <pallennellap17@gmail.com>
2020-03-15 17:37:06 +0900

    email 보내기전 보낼내용 테스트

--- 03 smtp/gmail.py
+++ 03 smtp/gmail.py
@@ -4,3 +4,14 @@ from email.mime.text import MIMEText
 sendEmail = 'pallenstudy@gmail.com'
 recvEmail = 'pallennellap17@gmail.com'
 password  = ''
+
+smtpName = 'smtp.gmail.com' #smtp 서버주소
+smtPort  = 587              #smtp 포트번호
+
+text = "python Email Test"
+msg  = MIMEText(text)
+
+msg['Subject'] = sendEmail
+msg['from'] = recvEmail
+print(msg.as_string())
+
