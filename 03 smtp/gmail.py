fe39757437637dc9626da5ff40f8dbc0b51bc0b7
pallenJ <pallennellap17@gmail.com>
2020-03-15 17:44:40 +0900

    구글 smtp 이메일 보내기 완료(비밀번호 일부러 비움)

--- 03 smtp/gmail.py
+++ 03 smtp/gmail.py
@@ -15,3 +15,9 @@ msg['Subject'] = sendEmail
 msg['from'] = recvEmail
 print(msg.as_string())
 
+s = smtplib.SMTP(smtpName, smtPort) # 메일 서버 연결(s가 구글서버와 통신하는 하는 통로)
+s.starttls() # TLS 보안처리
+s.login(sendEmail,password) # 계정 로그인
+s.sendmail(sendEmail, recvEmail, msg.as_string()) # 메일 전송, 문자열 반환
+s.close()
+
