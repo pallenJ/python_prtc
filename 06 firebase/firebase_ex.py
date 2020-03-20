097a34eab29d6353c3126fd9ae5940e266d7f76e
pallenJ <pallennellap17@gmail.com>
2020-03-20 13:00:49 +0900

    업데이트 실습

--- 06 firebase/firebase_ex.py
+++ 06 firebase/firebase_ex.py
@@ -1,7 +1,11 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, db
 
-cred = credentials.Certificate("mykey.json")
+# 파이어베이스 DB 인증 및 앱 초기화
+cred = credentials.Certificate("mykey.json") # 만든 새 프로젝트에서 가져온 인증키
 firebase_admin.initialize_app(cred,{
-    'databaseURL' : 'https://semicircle-16117.firebaseio.com/'
+    'databaseURL' : 'https://semicircle-16117.firebaseio.com/' # dbURL
 })
+
+ref = db.reference() # db 위치 지정
+ref.update({'준모':'호래이'}) # 해당변수가 없을경우 생성
