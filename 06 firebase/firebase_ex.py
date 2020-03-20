850e98ec325cdb1396b9947a8be7caf3fada729e
pallenJ <pallennellap17@gmail.com>
2020-03-20 13:05:52 +0900

    업데이트 실습2

--- 06 firebase/firebase_ex.py
+++ 06 firebase/firebase_ex.py
@@ -9,3 +9,10 @@ firebase_admin.initialize_app(cred,{
 
 ref = db.reference() # db 위치 지정
 ref.update({'준모':'호래이'}) # 해당변수가 없을경우 생성
+
+ref = db.reference('연습/파이썬') # 경로가 없으면 생성
+ref.update({'파이썬 레시피 웹 활용':'complete'})
+ref.update({'파이썬 괴식 레시피':'proceeding'})
+
+ref = db.reference()
+ref.update({'수강자' : ['구독자A','구독자B','구독자C','구독자D']}) # list 는 인덱스:요소 로 자리잡음
