f5968beeac19b03d2157415224d6f2e976c5be61
pallenJ <pallennellap17@gmail.com>
2020-03-23 10:39:42 +0900

    tqdm까지 적용 완료 다음은 압축

--- 02 selenium/naver_image.py
+++ 02 selenium/naver_image.py
@@ -1,12 +1,14 @@
 import os
 import time
+import zipfile
 from pprint import pprint
 from urllib.request import urlretrieve
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from tqdm import tqdm
 
-keyword = '마마무'
+keyword = input('수집할 키워드를 입력하세요 :')
 
 # 웹 접속 - 네이버 이미지 접속
 
@@ -22,8 +24,7 @@ driver.get(url)
 body = driver.find_element_by_css_selector('body') #body태그부분 가져옴
 for i in range(3):
     body.send_keys(Keys.PAGE_DOWN)
-    time.sleep(1)
-
+    time.sleep(1)# 아이피차단을 막기위한것
 
 # 이미지 링크수집
 
@@ -31,7 +32,7 @@ imgs = driver.find_elements_by_css_selector('img._img')# 태그명.클래스명
 
 rs = []
 
-for img in imgs:
+for img in tqdm(imgs):  # 이미지 다운로드 확인을 위한것
     if 'http' in img.get_attribute('src'):
         rs.append(img.get_attribute('src'))
 
@@ -39,13 +40,15 @@ driver.close()
 
 print('수집완료')
 if not os.path.isdir('./{}'.format(keyword)):
+    print('폴더생성')
     os.mkdir('./{}'.format(keyword))
 
-for idx, link in enumerate(rs) :
+for idx, link in tqdm(enumerate(rs)) :
     start = link.rfind('.')
     end   = link.rfind('&')
     #print(link[start:end])
     filetype = link[start:end] #파일타입 저장
     urlretrieve(link,'./{}/{}{}{}'.format(keyword,keyword,idx,filetype))
 
+print('다운로드 완료')
 
