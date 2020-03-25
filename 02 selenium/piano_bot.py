9983ed2eadb2c1ae1351a84b22b3e59652af42c4
pallenJ <pallennellap17@gmail.com>
2020-03-25 16:43:38 +0900

    좌표를 이용한 피아노게임 자동화 완료. 그러나 제어하기에 부적합.(pyaitoGUI 만으론 마우스 제어가 불완전함.)

--- 02 selenium/piano_bot.py
+++ 02 selenium/piano_bot.py
@@ -1,7 +1,12 @@
 import pyautogui as pg
 import time
+from PIL import ImageGrab
 
-
+taps = [(328,753),(607,753),(420,753),(517,753)]
 while True :
-    print(pg.position())
-    time.sleep(.5)
+    # print(pg.position())
+    # time.sleep(.5)
+    screen = ImageGrab.grab()
+    for tap in taps :
+        if (0,0,0) == screen.getpixel(tap) :
+            pg.click(*tap) #unpakking 연산자
