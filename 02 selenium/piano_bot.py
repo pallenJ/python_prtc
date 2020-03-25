13a3d1372e437d372587d48f26fa16e9ad657380
pallenJ <pallennellap17@gmail.com>
2020-03-25 16:55:47 +0900

    단축키로 멈춤 시작 완료

--- 02 selenium/piano_bot.py
+++ 02 selenium/piano_bot.py
@@ -1,12 +1,27 @@
 import pyautogui as pg
 import time
 from PIL import ImageGrab
+import keyboard
 
 taps = [(328,753),(607,753),(420,753),(517,753)]
-while True :
-    # print(pg.position())
-    # time.sleep(.5)
+state = False
+
+def play():
     screen = ImageGrab.grab()
     for tap in taps :
         if (0,0,0) == screen.getpixel(tap) :
             pg.click(*tap) #unpakking 연산자
+
+while True :
+    if not state and keyboard.is_pressed('a'):
+        state = True
+        print('Start!')
+    elif state and keyboard.is_pressed('s'):
+        state = False
+        print('Stop!')
+        break;
+    if state:
+        play()
+    # print(pg.position())
+    # time.sleep(.5)
+
