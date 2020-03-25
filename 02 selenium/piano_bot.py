4eab0ea5ef17840128570ed5a22aef05a3fca986
pallenJ <pallennellap17@gmail.com>
2020-03-25 16:58:47 +0900

    화면 특정부분만 캡쳐해서 클릭 실습

--- 02 selenium/piano_bot.py
+++ 02 selenium/piano_bot.py
@@ -6,8 +6,11 @@ import keyboard
 taps = [(328,753),(607,753),(420,753),(517,753)]
 state = False
 
+width , height = pg.size()
+box = (0,0,width/2,height)
+
 def play():
-    screen = ImageGrab.grab()
+    screen = ImageGrab.grab(box)
     for tap in taps :
         if (0,0,0) == screen.getpixel(tap) :
             pg.click(*tap) #unpakking 연산자
